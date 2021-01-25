654b36bb65cc9b25f50731add38c1595953662a6
Elijah Wines <wines.elijah@gmail.com>
2021-01-25 09:48:21 -0500

    game functionality complete

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-// click on the start/reset
 window.addEventListener('load', (e) => {
-    const startReset = document.querySelector("div#start-reset");
 
+    // checks the text content of the start/reset button
     const playing = () => {
         if(startReset.innerText === "Start game"){
             return true
@@ -10,11 +9,13 @@ window.addEventListener('load', (e) => {
         }
     }
 
+    // sets the score to zero whenever the game has begun
     const setScoreToZero = () => {
         const scoreValue = document.getElementById('score-value');
         scoreValue.innerText = "0"
     }
 
+    // changes the buttons inner text when the game is started
     const changeStartResetButton = () => {
         if(startReset.innerText === "Start game"){
             startReset.innerText = "Reset"
@@ -23,6 +24,7 @@ window.addEventListener('load', (e) => {
         }
     }
 
+    // generates the questions and answers
     const generateQA = () => {
         const correctDiv = document.getElementById('correct');
         if(correctDiv.style.display = "block"){
@@ -37,6 +39,7 @@ window.addEventListener('load', (e) => {
         generateQuestion();
     }
 
+    // generates a new question with multiples up to 12
     const generateQuestion = () => {
         const qBox = document.getElementById('question');
         let n1 = Math.floor(Math.random() * 12) + 1;
@@ -45,6 +48,7 @@ window.addEventListener('load', (e) => {
         generateAnswers(n1 * n2);
     }
 
+    // shuffles the array of 1-4 to randomly choose which answer box contains the correct answer
     const shuffle = (arr) => {
         var currentIndex = arr.length, temporaryValue, randomIndex;
 
@@ -63,6 +67,7 @@ window.addEventListener('load', (e) => {
         return arr;
     }    
 
+    // generates the answers and randomly chooses which box holds the correct answer
     const generateAnswers = (ans) => {
         let nums = [1,2,3,4];
         let shuffledNums = shuffle(nums);
@@ -80,6 +85,7 @@ window.addEventListener('load', (e) => {
         addAnswerToRandomBox(ans);
     }
 
+    // adds the anser to the random box
     const addAnswerToRandomBox = (ans) => {
         let ansBoxNumber = String(Math.floor(Math.random() * 4) + 1);
         let ansBox = document.getElementById(`box${ansBoxNumber}`)
@@ -88,10 +94,12 @@ window.addEventListener('load', (e) => {
         ansBox.addEventListener("click", rightAnswerClicked);
     }
 
+    // triggers when the user clicks the wrong answer
     const wrongAnswerClicked = () => {
         flashWrongDiv()
     }
 
+    // flashes the 'wrong' div to the user for 250ms
     const flashWrongDiv = () => {
         let tryAgainDiv = document.getElementById('wrong');
         tryAgainDiv.style.opacity = "100";
@@ -102,6 +110,7 @@ window.addEventListener('load', (e) => {
         }, 250)
     }
 
+    // triggered when the user clicks the right answer
     const rightAnswerClicked = () => {
         flashCorrectDiv();
         var score = document.getElementById('score-value');
@@ -109,47 +118,65 @@ window.addEventListener('load', (e) => {
         setTimeout(generateQA, 250)
     }
 
+    // adds a point to the users score
     const addOneToScore = (score) => {
         let newScore = parseInt(score.innerText, 10) + 1;
         score.innerText = newScore;
     }
 
+    // flashes the 'correct' div to the user for 250ms
     const flashCorrectDiv = () => {
         let correctDiv = document.getElementById('correct');
         correctDiv.style.opacity = "100";
         correctDiv.style.zIndex = "2";
     }
 
+    //displays the countdown and triggers the timer
+    const startCountdown = () => {
+        let timeDiv = document.getElementById('time-remaining');
+        timeDiv.style.display = "block";
+        timer();
+    }
+
+    // decreases the time left by one every second
+    const timer = () => {
+        let timeEl = document.getElementById('time-remaining-value');
+        let time = parseInt(timeEl.innerText, 10);
+        let interval = setInterval(() => {
+            if(time === 0){
+                clearInterval(interval);
+                console.log('times up');
+                showScore();
+            } else {
+                time -= 1;
+                timeEl.innerText = time;
+            }
+        }, 1000)
+    }
+
+    // shows the users score after time runs out
+    const showScore = () => {
+        let scoreBoard = document.getElementById('game-over');
+        scoreBoard.style.display = "block";
+        let gameOverScore = document.getElementById('game-over-score');
+        let score = document.getElementById('score-value');
+        gameOverScore.innerText = score.innerText;
+    }
+
+    // triggers the game start when clicked
+    const startReset = document.getElementById("start-reset");
+
     startReset.addEventListener("click", (e) => {
-        // if we are playing
         if(!playing()){
-            // reload page
             console.log('game ended')
             location.reload()
         } else {
             console.log('game started')
-            // set score to 0
             setScoreToZero();
-            // change button to reset
             changeStartResetButton();
-            // generate Q&A
             generateQA();
-            // show countdown
-            // reduce time by 1 every second
-            // check if there is time left
-                //yes-continue
-                //no-game over
+            startCountdown();
         }
     })
-})
-        
 
-// click on answer box
-    // if playing
-        // check if answer is correct
-            //yes- 
-                // increase score by one
-                // generate new Q&A
-                // show correct box
-            //no-
-                //generate try again box
+})
